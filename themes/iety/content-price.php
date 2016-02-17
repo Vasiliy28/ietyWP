98c667ac8edfefc3477aa3079c570aaad03451dd
Vasiliy28 <Vasiliy13022015@gmail.com>
2016-02-17 15:10:30 +0300

    new

--- themes/iety/content-price.php
+++ themes/iety/content-price.php
@@ -6,29 +6,43 @@
  * Time: 11:14
  */
 
+$query_header = new WP_Query(array(
+    'post_type' => 'price',
+    'orderby' => array(
+        'menu_order' => 'ASC',
+    ),
+    'type_price' => 'header',
+    'posts_per_page' => 1,
+));
+
 $query_price = new WP_Query(array(
     'post_type' => 'price',
     'orderby' => array(
         'menu_order' => 'DASC',
     ),
-    'posts_per_page' =>  4,
+    'posts_per_page' => 4,
 ));
 
 ?>
-<section id="<?php echo the_ID(); ?>" class="wrapperPrice backgroundLight">
+
+<?php if ($query_header->have_posts()): ?>
+<?php while ($query_header->have_posts()):$query_header->the_post() ?>
+<section id="<?php echo the_ID(); ?>" class="wrapperPrice <?php echo(the_field('background_post')); ?>">
     <span></span><span></span>
     <div class="container">
         <div class="row">
             <section class="prices clearfix">
-
+                <?php endwhile; ?>
+                <?php endif; ?>
+                <?php wp_reset_query(); ?>
                 <?php if ($query_price->have_posts()): ?>
-                <?php while ($query_price->have_posts()):$query_price->the_post() ?>
+                    <?php while ($query_price->have_posts()):$query_price->the_post() ?>
                         <div class="col-xs-6 col-md-3">
                             <article class="priceing">
                                 <header>
                                     <h1><?php the_field('name_card') ?></h1>
                                 </header>
-                                <div class="price"><?php the_field('how_much')?>&#36;</div>
+                                <div class="price"><?php the_field('how_much') ?>&#36;</div>
                                 <p><?php the_field('some_info') ?></p>
                                 <?php the_field('icon_card') ?>
                                 <?php the_field('list') ?>
@@ -38,10 +52,7 @@ $query_price = new WP_Query(array(
                     <?php endwhile; ?>
                 <?php endif; ?>
                 <?php wp_reset_query(); ?>
-
-
             </section>
-
         </div>
     </div>
 </section>
--- themes/iety/functions.php
+++ themes/iety/functions.php
@@ -6,7 +6,6 @@
 
 show_admin_bar(false);
 
-
 /**
  * Slider
  */
@@ -26,7 +25,8 @@ if ( ! function_exists( 'iety_setup' ) ) :
 
 // This theme uses wp_nav_menu() in one location.
         register_nav_menus( array(
-            'headerMenuIety' =>( 'Header Menu Iety' )
+            'headerMenuIety' =>( 'Header Menu Iety' ),
+            'footerMenuIety' =>( 'Footer Menu Iety' )
         ) );
 
         class True_Walker_Nav_Menu extends Walker_Nav_Menu {
